e435677ce19c64fab56bfaa28a06e429a65979e2
Doug <dmkcodes@gmail.com>
2023-03-27 12:28:57 -0400

    add navbar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,43 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { Navbar } from 'reactstrap';
+import { 
+    Navbar, 
+    NavbarToggler, 
+    Nav, 
+    NavItem, 
+    Collapse 
+} from 'reactstrap';
 import RegisterLoginModal from './RegisterLoginModal';
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <Navbar dark color='success' sticky='top' expand='md'>
             <NavLink className='nav-link ms-md-5' to='/' style={{ color: 'white' }}>
-                <h3 className='mt-1'>MERN Boilerplate</h3>
+                <h3 className='mt-1'>EcoUPDATE</h3>
             </NavLink>
-            <RegisterLoginModal />
+            <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
+            <Collapse isOpen={menuOpen} navbar>
+                <Nav className='ms-auto' navbar>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/'>
+                            Policy
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link' to='/'>
+                            News
+                        </NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink className='nav-link me-3' to='/'>
+                            Data
+                        </NavLink>
+                    </NavItem>
+                </Nav>
+                <RegisterLoginModal />
+            </Collapse>
         </Navbar>
     );
 };
