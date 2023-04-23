a386cbd6d098e9b3ba5b251a1ab58124460a2415
Doug <dmkcodes@gmail.com>
2023-04-23 13:13:33 -0400

    update home page

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,7 +3,6 @@ import { Container } from 'reactstrap';
 const HomePage = () => {
     return (
         <Container>
-            <h2 className='mt-3'>EcoUPDATE</h2>
             <p>Welcome to Biodiversity 365! This platform is a one-stop shop for policy, news, and data concerning biodiversity and conservation.</p>
         </Container>
     );
